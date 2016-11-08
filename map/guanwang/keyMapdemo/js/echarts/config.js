5c9b218f1847edc3c71c2e82ae19cb830d857804
wangweiwei <will_workhard@163.com>
2016-11-08 20:41:38 +0800

    no message

---
+++ map/guanwang/keyMapdemo/js/echarts/config.js
@@ -0,0 +1,21 @@
+require.config({
+    paths: {
+        'geoJson': '../geoData/geoJson',
+        'data': './data',
+        'map': '../map',
+        'extension': '../extension'
+    },
+    packages: [
+        {
+            main: 'echarts',
+            location: '../src',
+            name: 'echarts'
+        },
+        {
+            main: 'zrender',
+            location: '../../zrender/src',
+            name: 'zrender'
+        }
+    ]
+    // urlArgs: '_v_=' + +new Date()
+});
